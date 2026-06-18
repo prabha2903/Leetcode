e42867a76f864947c391e97641dbd38ca8577c50
Prabhacod <prabha29070607@gmail.com>
2026-06-18 15:46:37 +0530

    1344. Angle Between Hands of a Clock

---
+++ 1344. Angle Between Hands of a Clock.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        double minAngle = minutes * 6.0;
+        double hourAngle = (hour%12) * 30.0 + minutes * 0.5;
+        double diff = Math.abs(hourAngle-minAngle);
+        return Math.min(diff,360.0-diff);
+    }
+}
